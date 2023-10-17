fa54e441f0d5fe12a693c0f8282a008c0705770e
maulana-kurniawan <117083636+maulana-kurniawan@users.noreply.github.com>
2023-10-17 19:42:38 +0700

    Fix for PHP 8.2

--- system/libraries/Xmlrpc.php
+++ system/libraries/Xmlrpc.php
@@ -1153,7 +1153,7 @@ class XML_RPC_Message extends CI_Xmlrpc
 		//-------------------------------------
 
 		$parser = xml_parser_create($this->xmlrpc_defencoding);
-		$pname = (string) $parser;
+		$pname = spl_object_id($parser);
 		$this->xh[$pname] = array(
 			'isf'		=> 0,
 			'ac'		=> '',
@@ -1281,7 +1281,7 @@ class XML_RPC_Message extends CI_Xmlrpc
 	 */
 	public function open_tag($the_parser, $name)
 	{
-		$the_parser = (string) $the_parser;
+		$the_parser = spl_object_id($the_parser);
 
 		// If invalid nesting, then return
 		if ($this->xh[$the_parser]['isf'] > 1) return;
@@ -1382,7 +1382,7 @@ class XML_RPC_Message extends CI_Xmlrpc
 	 */
 	public function closing_tag($the_parser, $name)
 	{
-		$the_parser = (string) $the_parser;
+		$the_parser = spl_object_id($the_parser);
 
 		if ($this->xh[$the_parser]['isf'] > 1) return;
 
@@ -1516,7 +1516,7 @@ class XML_RPC_Message extends CI_Xmlrpc
 	 */
 	public function character_data($the_parser, $data)
 	{
-		$the_parser = (string) $the_parser;
+		$the_parser = spl_object_id($the_parser);
 
 		if ($this->xh[$the_parser]['isf'] > 1) return; // XML Fault found already
 
